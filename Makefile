08f39029950ceb4f6c52b7ef32b589285dc397b6
kevin91nl <kevin91nl@gmail.com>
2019-06-12 12:51:53 +0200

    - Added tests for the external data loader. - Configuration for Pylint updated.

--- Makefile
+++ Makefile
@@ -4,5 +4,8 @@ push:
 	docker push kevin91nl/entity-salience
 checks:
 	flake8
-	find . -iname "*.py" ! -wholename "*node_modules*" ! -wholename "*migrations*" ! -wholename "*.git*" ! -wholename "*test_data*" ! -wholename "*tests*" ! -wholename "*manage.py*" ! -wholename "*sandbox*" | xargs pylint
-	pydocstyle --convention=numpy --match-dir "^(?!migrations|node_modules|\.git|test_data|tests|sandbox|Trash).*"
+	touch __init__.py
+	trap "rm __init__.py" EXIT
+	pylint /code
+	pydocstyle --convention=numpy --match-dir "^(?!migrations|node_modules|\.git|sandbox|Trash).*"
+	pytest tests
--- tests/test_dataset_loader.py
+++ tests/test_dataset_loader.py
@@ -1,7 +1,79 @@
+import json
 import unittest
+import dataset.loader
 
 
-class Test(unittest.TestCase):
+class TestDatasetLoader(unittest.TestCase):
+    class FakeResponse:
+        """The FakeResponse class makes a fake response by a self-specified status code and content."""
 
-    def test_example(self):
-        self.assertTrue(False)
+        def __init__(self, status_code, content):
+            self.status_code = status_code
+            self.content = content.encode('utf-8')
+
+    class FakeRequests:
+        """The FakeRequests class fakes the requests library and has a custom get() method which always delivers the
+        given FakeResponse."""
+
+        def __init__(self, fake_response):
+            self.fake_response = fake_response
+
+        def get(self, url):
+            return self.fake_response
+
+    def test_get_csv(self):
+        # Fake a response (for any given URL)
+        dataset.loader.requests = TestDatasetLoader.FakeRequests(TestDatasetLoader.FakeResponse(
+            status_code=200,
+            content='header1,header2' + "\n" + 'cell1,cell2'
+        ))
+
+        # Setup the external dataloader
+        loader = dataset.loader.ExternalDataLoader()
+        loader.files['test-csv'] = {
+            'path': 'test.csv',
+            'parser': dataset.loader.ExternalDataLoader.parse_csv
+        }
+
+        # Try to fetch the file
+        result = loader.get('test-csv', ignore_cache=True)
+
+        # Check whether the content is correct
+        self.assertListEqual(result.columns.tolist(), ['header1', 'header2'])
+
+    def test_get_json(self):
+        # Fake a response (for any given URL)
+        dataset.loader.requests = TestDatasetLoader.FakeRequests(TestDatasetLoader.FakeResponse(
+            status_code=200,
+            content=json.dumps({'hello': {0: 'world'}})
+        ))
+
+        # Setup the external dataloader
+        loader = dataset.loader.ExternalDataLoader()
+        loader.files['test-json'] = {
+            'path': 'test.json',
+            'parser': dataset.loader.ExternalDataLoader.parse_json
+        }
+
+        # Try to fetch the file
+        result = loader.get('test-json', ignore_cache=True)
+
+        # Check whether the content is correct
+        self.assertListEqual(result.columns.tolist(), ['hello'])
+
+    def test_404_response(self):
+        # Fake a 404 (file not found) response
+        dataset.loader.requests = TestDatasetLoader.FakeRequests(TestDatasetLoader.FakeResponse(
+            status_code=404,
+            content=""
+        ))
+
+        # Setup the external dataloader
+        loader = dataset.loader.ExternalDataLoader()
+
+        # Try to fetch any file (which will encounter a 404 by the fake request)
+        first_file = list(loader.files.keys())[0]
+
+        # Trying to fetch the file should raise a ValueError
+        with self.assertRaises(ValueError):
+            loader.get(first_file, ignore_cache=True)
