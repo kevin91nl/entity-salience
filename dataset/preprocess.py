1b437a13444dc453f7e53860c80e88073b1347a8
kevin91nl <kevin91nl@gmail.com>
2019-06-13 09:22:43 +0200

    - Splitting functionality into a representation class and a class containing operations about tokenization.

--- dataset/preprocess.py
+++ dataset/preprocess.py
@@ -1,7 +1,34 @@
 """The preprocess packages contains preprocessing methods."""
 
 
-class Tokenize:
+class Tokens(list):
+    """A class holding the tokens of a text."""
+
+    def __str__(self):
+        """Create the string representation of the tokens.
+
+        Returns
+        -------
+        str
+            The string representation of the tokens.
+        """
+        quotes_tokens = [f'"{token}"' for token in self]
+        tokens_display = ', '.join(quotes_tokens) if len(quotes_tokens) < 10 else ', '.join(
+            quotes_tokens[:3]) + ', ..., ' + ', '.join(quotes_tokens[-3:])
+        return "Tokens ({}): [{}]".format(len(self), tokens_display)
+
+    def __repr__(self):
+        """Create the string representation of the tokens.
+
+        Returns
+        -------
+        str
+            The string representation of the tokens.
+        """
+        return self.__str__()
+
+
+class Tokenizer:
     """Class used for the tokenizing a text."""
 
     @staticmethod
@@ -20,65 +47,20 @@ class Tokenize:
         """
         return text.split()
 
-    def __init__(self, text, tokenizer_method=None):
+    def __init__(self, tokenizer_method=None):
+        self.tokenizer_method = tokenizer_method if tokenizer_method is not None else self.default_tokenizer
+
+    def __call__(self, text):
         """Tokenize a text.
 
         Parameters
         ----------
         text : str
             Text to tokenize
-        tokenizer_method : function
-            Method to use to convert a text into tokens.
-        """
-        self.tokenizer_method = tokenizer_method if tokenizer_method is not None else self.default_tokenizer
-        self.tokens = list(self.tokenizer_method(text))
-
-    def __str__(self):
-        """Create the string representation of the tokens.
 
         Returns
         -------
-        str
-            The string representation of the tokens.
+        Tokens
+            A class holding the tokens.
         """
-        quotes_tokens = [f'"{token}"' for token in self.tokens]
-        tokens_display = ', '.join(quotes_tokens) if len(quotes_tokens) < 10 else ', '.join(
-            quotes_tokens[:3]) + ', ..., ' + ', '.join(quotes_tokens[-3:])
-        return "Tokens ({}): [{}]".format(len(self.tokens), tokens_display)
-
-    def __repr__(self):
-        """Create the string representation of the tokens.
-
-        Returns
-        -------
-        str
-            The string representation of the tokens.
-        """
-        return self.__str__()
-
-    def __getitem__(self, item):
-        """Retrieve a token.
-
-        Parameters
-        ----------
-        item : int
-            Index of the token.
-
-        Returns
-        -------
-        str
-            The token at the given. index.
-        """
-        return self.tokens[item]
-
-    def __setitem__(self, key, value):
-        """Set a token.
-
-        Parameters
-        ----------
-        key : int
-            The index of the token.
-        value : str
-            The new value of the token.
-        """
-        self.tokens[key] = value
+        return Tokens(self.tokenizer_method(text))
