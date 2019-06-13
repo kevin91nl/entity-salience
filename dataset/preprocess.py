874c5fea37303560aebe08e73bde01da9997554d
kevin91nl <kevin91nl@gmail.com>
2019-06-13 09:31:20 +0200

    - Added a token representation class.

--- dataset/preprocess.py
+++ dataset/preprocess.py
@@ -1,8 +1,54 @@
 """The preprocess packages contains preprocessing methods."""
 
 
+class Token:
+    """A class for representing a token."""
+
+    def __init__(self, text):
+        """Initialize a token.
+
+        Parameters
+        ----------
+        text : str
+            The label of the token.
+        """
+        self.text = text
+
+    def __str__(self):
+        """Create the string representation of a token.
+
+        Returns
+        -------
+        str
+            The string representation of a tokens.
+        """
+        return self.text
+
+    def __repr__(self):
+        """Create the string representation of a token.
+
+        Returns
+        -------
+        str
+            The string representation of a tokens.
+        """
+        return self.__str__()
+
+
 class Tokens(list):
     """A class holding the tokens of a text."""
+
+    def __init__(self, raw_tokens):
+        """Initialize the list of tokens with raw tokens (strings).
+
+        Parameters
+        ----------
+        raw_tokens : list
+            A list of tokens (strings).
+        """
+        super().__init__()
+        for raw_token in raw_tokens:
+            self.append(Token(text=raw_token))
 
     def __str__(self):
         """Create the string representation of the tokens.
